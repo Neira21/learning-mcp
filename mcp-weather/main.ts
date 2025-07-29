0f7385d1dbe0665f5b1d9599b181566875140432
Alvaro Neira <alvaroneira98@gmail.com>
2025-07-29 09:32:40 -0500

    practice api weather

--- mcp-weather/main.ts
+++ mcp-weather/main.ts
@@ -1,34 +1,62 @@
-import { McpServer } from '@modelcontextprotocol/sdk/server/mcp.js';
+import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
 import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
-import { z } from 'zod';
-
+import { z } from "zod";
 
 const server = new McpServer({
-  name: 'Weather Server',
-  version: '1.0.0',
-})
-
+  name: "Weather Server",
+  version: "1.0.0",
+});
 
 server.tool(
-  'get-weather',
-  'Fetch weather data for a given city',
+  "get-weather",
+  "Fetch weather data for a given city",
   {
-    city: z.string().describe('Name of the city to fetch weather data for'),
+    city: z.string().describe("Name of the city to fetch weather data for"),
   },
   async ({ city }) => {
-    return {
-      content: [
-        {
-          type: 'text',
-          text: `Fetching weather data for ${city} is snowwing!`,
-        }
-        
-      ]
+    try {
+      const response = await fetch(
+        `https://geocoding-api.open-meteo.com/v1/search?name=${city}&count=10&language=en&format=json`
+      );
+      const data = await response.json();
+
+      if (data.results.length === 0) {
+        return {
+          content: [
+            {
+              type: "text",
+              text: `No results found for city: ${city}`,
+            },
+          ],
+        };
+      }
+      const { latitude, longitude } = data.results[0];
+      const weatherResponse = await fetch(
+        `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&hourly=temperature_2m,precipitation,apparent_temperature,relative_humidity_2m&forecast_days=1`
+      );
+
+      const weatherData = await weatherResponse.json();
+
+      return {
+        content: [
+          {
+            type: "text",
+            text: JSON.stringify(weatherData, null, 2),
+          },
+        ],
+      };
+    } catch (error) {
+      return {
+        content: [
+          {
+            type: "text",
+            text: `Error fetching weather data: ${error.message}`,
+          },
+        ],
+      };
     }
   }
-)
+);
 
 const transport = new StdioServerTransport();
-server.connect(transport)
-
-
+server.connect(transport);
